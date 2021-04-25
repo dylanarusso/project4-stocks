550372ddff006854dfa485f04dda9708e9c98461
Dylan Russo <dylan.a.russo@gmail.com>
2021-04-25 14:50:20 -0400

    Created activeSearch for search bar

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
 
+import {useState} from 'react'
+
 function App() {
+
+  const [activeSearch, setActiveSearch] = useState();
+
+  const updateActiveSearch = (ev) => {
+    console.log(ev.currentTarget.value);
+    setActiveSearch(ev.currentTarget.value)
+  };
+
+  const searchStock = async () => {
+    let res = await fetch(`http://localhost:3000/api/v1/search/${activeSearch}`);
+    let json = await res.json();
+    console.log(json);
+  };
+
+
   return (
     <>
 
@@ -11,11 +28,36 @@ function App() {
           <h1 className={'text-center text-4xl p-5'}>Paper Trader</h1>
         </div>
 
-        <div className={'col-span-7 border-gray-700 h-96 border-r-4'}>
-              box2
+        <div className={'col-span-12 md:col-span-7 border-gray-700 h-96 border-r-4'}>
+
+          <div className={'grid grid-cols-12'}>
+            <div className={'col-span-12 md:col-span-6 h-32 border p-5'}>
+              <input value={activeSearch} onChange={updateActiveSearch} type='search' className={'border w-2/3 p-3 rounded-full'}/>
+
+              <button onClick={searchStock} className={'border w-1/3 bg-blue-500 text-white cursor-pointer'}>Search</button>
+
+            </div>
+          
+            <div className={'hidden md:grid col-span-6 h-32 border p-5'}>
+              Empty
+            </div>
+
+            <div className={'col-span-6 h-32 border p-5'}>
+              Stock name
+
+            </div>
+
+            <div className={'col-span-6 h-32 border p-5'}>
+              Buy button
+
+            </div>
+
+          </div>
+
+
         </div>
 
-        <div className={'col-span-5 h-96'}>
+        <div className={'col-span-12 md:col-span-5 h-96'}>
               box3
         </div>
 
