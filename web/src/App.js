69891da798cece3820be77c4af1dd39227078008
Dylan Russo <dylan.a.russo@gmail.com>
2021-04-29 18:53:29 -0400

    Change web style

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 import {useState} from 'react'
+// import { search } from '../../api/app'
 
 function App() {
 
   const [activeSearch, setActiveSearch] = useState();
+
 
   const updateActiveSearch = (ev) => {
     console.log(ev.currentTarget.value);
@@ -15,6 +17,8 @@ function App() {
     let res = await fetch(`http://localhost:3000/api/v1/search/${activeSearch}`);
     let json = await res.json();
     console.log(json);
+
+  
   };
 
 
@@ -24,17 +28,17 @@ function App() {
 
       <div className={'grid grid-cols-12'}>
        
-        <div className={'col-span-12 border border-gray-700 border-b-4'}>
+        <div className={'col-span-12 border border-white border-b-4 bg-gray-300'}>
           <h1 className={'text-center text-4xl p-5'}>Paper Trader</h1>
         </div>
 
-        <div className={'col-span-12 md:col-span-7 border-gray-700 h-96 border-r-4'}>
+        <div className={'col-span-12 md:col-span-7 border-white h-96 border-r-4 bg-gray-300'}>
 
           <div className={'grid grid-cols-12'}>
             <div className={'col-span-12 md:col-span-6 h-32 border p-5'}>
               <input value={activeSearch} onChange={updateActiveSearch} type='search' className={'border w-2/3 p-3 rounded-full'}/>
 
-              <button onClick={searchStock} className={'border w-1/3 bg-blue-500 text-white cursor-pointer'}>Search</button>
+              <button onClick={searchStock} className={'border w-1/3 bg-blue-500 text-white cursor-pointer rounded'}>Search</button>
 
             </div>
           
@@ -45,10 +49,12 @@ function App() {
             <div className={'col-span-6 h-32 border p-5'}>
               Stock name
 
+
             </div>
 
             <div className={'col-span-6 h-32 border p-5'}>
               Buy button
+
 
             </div>
 
@@ -57,7 +63,7 @@ function App() {
 
         </div>
 
-        <div className={'col-span-12 md:col-span-5 h-96'}>
+        <div className={'col-span-12 md:col-span-5 h-96 bg-gray-300'}>
               box3
         </div>
 
