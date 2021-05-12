252bd0fcb2d4eab9a3b5f705ceeaa027e1a6b744
Dylan Russo <dylan.a.russo@gmail.com>
2021-05-12 12:48:47 -0400

    fixed buy button to work with wallet

---
+++ web/src/Components/Chart.js
@@ -0,0 +1,29 @@
+// import React, {Component} from 'react';
+// import {Bar, Line, Pie} from 'react-chartjs-2';
+
+// class Chart extends Component{
+//     constructor(){
+//         super(props);
+//         this.state = {
+//             chartData:{
+
+//             }
+//         }
+//     }
+    
+    
+//     render(){
+//         return (
+//             <div className="chart">
+
+//                 <Bar
+// 	                data={data}
+// 	                options={{ maintainAspectRatio: false }}
+//                 />
+
+//             </div>
+//         )
+//     }
+// }
+
+// export default Chart;
